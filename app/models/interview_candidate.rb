f5978cb840a4e7323754175de38bd988f2d50345
Fatimah Sanni <fatimah.sanni@gmail.com>
2015-07-02 09:05:31 +0100

    Star rating added for Appraisal and Interview Candidates

--- app/models/interview_candidate.rb
+++ app/models/interview_candidate.rb
@@ -1,4 +1,10 @@
 class InterviewCandidate < ActiveRecord::Base
   belongs_to :interview
   validates_numericality_of :education, :job, :work, :communication, :initiative, :enthusiasm, :company, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
+
+  def calc_average
+  	total = self.education + self.job+self.work+self.communication+self.initiative+self.enthusiasm+self.company
+  	average = (total/7.0).round(2)
+  	return average
+  end
 end
